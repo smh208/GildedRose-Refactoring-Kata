1b260f3fee1281970903a8a206b602741a2ba4a9
Nabeel Herzalla <nabeel.herzalla@sherwin.com>
2023-11-06 13:07:44 -0500

    Adding more unit tests

--- csharpcore/GildedRoseTests/GildedRoseTest.cs
+++ csharpcore/GildedRoseTests/GildedRoseTest.cs
@@ -6,14 +6,6 @@ namespace GildedRoseTests;
 
 public class GildedRoseTest
 {
-    //[Test]
-    //public void Foo()
-    //{
-    //    var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-    //    var app = new GildedRose(items);
-    //    app.UpdateQuality();
-    //    Assert.AreEqual("fixme", items[0].Name);
-    //}
 
     [Test]
     public void AgedBrieLessThanZeroHigherQuality()
@@ -24,7 +16,7 @@ public class GildedRoseTest
         Assert.AreEqual(47, items[0].Quality);
     }
     [Test]
-    public void AgedBrieGreaterThanZeroSellInHigherQuality()
+    public void AgedBrieGreaterThanZeroSellInWithOneQualityIncrease()
     {
         var items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 2, Quality = 45 } };
         var app = new GildedRose(items);
@@ -41,12 +33,45 @@ public class GildedRoseTest
         Assert.AreEqual(1, items[0].SellIn);
     }
 
-    [Test]
-    public void SellByPassed()
+    [Test] public void SulfurasItemNeverSellNeverAged()
     {
-        //var items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 2, Quality = 50 } };
-        //var app = new GildedRose(items);
-        //app.UpdateQuality();
-        //Assert.AreEqual(1, items[0].SellIn);
+        var items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 2, Quality = 50 } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.AreEqual(2, items[0].SellIn);
+        Assert.AreEqual(50, items[0].Quality);
+
+    }
+    
+    [Test]
+    public void BackStageSellLessThan11DaysAnd50Quality()
+    {
+        var items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 2, Quality = 50 } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.AreEqual(1, items[0].SellIn);
+
+    }
+
+    [Test]
+    public void BackStageSellGreaterThan6DaysAndLessThan50Quality()
+    {
+        var items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 7, Quality = 45 } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.AreEqual(6, items[0].SellIn);
+        Assert.AreEqual(47, items[0].Quality);
+
+    }
+
+    [Test]
+    public void BackStageSellLessThan6DaysAndLessThan50Quality()
+    {
+        var items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 2, Quality = 45 } };
+        var app = new GildedRose(items);
+        app.UpdateQuality();
+        Assert.AreEqual(1, items[0].SellIn);
+        Assert.AreEqual(48, items[0].Quality);
+
     }
 }
